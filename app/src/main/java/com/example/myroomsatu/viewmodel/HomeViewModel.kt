ac1569ad4b80bf4f048700cd21d56d6cb6e4722d
Arsyan <syandi.2901@gmail.com>
2025-11-27 01:34:38 -0800

    membuat code untuk HomeViewModel.kt di view model

---
+++ app/src/main/java/com/example/myroomsatu/viewmodel/HomeViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.myroomsatu.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.myroomsatu.repositori.RepositoriSiswa
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
