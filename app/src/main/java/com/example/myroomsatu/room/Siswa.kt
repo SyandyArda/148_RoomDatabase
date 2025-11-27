5c9323f2612f2232d9e12c8dbe941e536af24f9c
Arsyan <syandi.2901@gmail.com>
2025-11-27 00:55:14 -0800

    membuat code untuk siswa.kt  di room

--- app/src/main/java/com/example/myroomsatu/room/Siswa.kt
+++ app/src/main/java/com/example/myroomsatu/room/Siswa.kt
@@ -1,4 +1,13 @@
 package com.example.myroomsatu.room
 
-@entity Siswa {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity("tblSiswa")
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
