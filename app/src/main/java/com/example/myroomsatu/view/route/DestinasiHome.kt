42be12761c77e5f51cc8d6b94a4573a80881e7b9
Arsyan <syandi.2901@gmail.com>
2025-11-27 04:47:13 -0800

    membuat code untuk Destinasientry.kt di route

--- app/src/main/java/com/example/myroomsatu/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/myroomsatu/view/route/DestinasiHome.kt
@@ -1,5 +1,9 @@
 package com.example.myroomsatu.view.route
 
+import com.example.myroomsatu.R
+
 object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
 
 }
