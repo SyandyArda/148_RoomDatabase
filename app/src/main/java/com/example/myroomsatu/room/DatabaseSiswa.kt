7a72113bc30c2e2aec75ce96bba4c45e84154ea2
Arsyan <syandi.2901@gmail.com>
2025-11-27 01:06:39 -0800

    membuat code untuk DatabaseSiswa.kt  di room

--- app/src/main/java/com/example/myroomsatu/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/room/DatabaseSiswa.kt
@@ -1,5 +1,21 @@
 package com.example.myroomsatu.room
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
-@Database
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase(){
+    abstract fun siswaDao() : SiswaDao
+
+    companion object{
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return (Instance?: synchronized(this){
+                Room databaseBuilder()
+            })
+        }
+    }
+}
