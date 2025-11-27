ffdbf81207b9d6e3932846878ba48a214c27e1a6
Arsyan <syandi.2901@gmail.com>
2025-11-27 01:57:22 -0800

    tambahan code untuk EntryViewModel.kt di view model

--- app/src/main/java/com/example/myroomsatu/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/myroomsatu/viewmodel/EntryViewModel.kt
@@ -1,6 +1,27 @@
 package com.example.myroomsatu.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.myroomsatu.repositori.RepositoriSiswa
 import com.example.myroomsatu.room.Siswa
+
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    /**
+     * Berisi status Siswa saat ini
+     */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+    /* Fungsi untuk memvalidasai input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+    fun updateUiState(detailSiswa: DetailSiswa) {
+
+    }
+}
 
 data class DetailSiswa(
     val id: Int = 0,
