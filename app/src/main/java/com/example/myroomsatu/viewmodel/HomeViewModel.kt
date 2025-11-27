33e23a5671fc982158b23f9a8e4ae9116c7139c8
Arsyan <syandi.2901@gmail.com>
2025-11-27 01:37:37 -0800

    tambahan code untuk HomeViewModel.kt di view model

--- app/src/main/java/com/example/myroomsatu/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/myroomsatu/viewmodel/HomeViewModel.kt
@@ -1,11 +1,23 @@
 package com.example.myroomsatu.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.myroomsatu.repositori.RepositoriSiswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
 
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+
+    val homeUiState: StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream()
+                                                .filterNotNull()
+        .map { HomeUiState(listSiswa = it.toList()) }
+        .stateIn(scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUiState())
 }
