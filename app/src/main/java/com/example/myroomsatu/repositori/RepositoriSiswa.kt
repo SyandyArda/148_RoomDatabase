45799904268e70613370d9d3643f4d98209864dc
Arsyan <syandi.2901@gmail.com>
2025-12-04 00:15:53 -0800

    nambahb fun di repo

--- app/src/main/java/com/example/myroomsatu/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/myroomsatu/repositori/RepositoriSiswa.kt
@@ -7,6 +7,9 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun  insertSiswa(siswa: Siswa)
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun deleteSiswa (siswa: Siswa)
+    suspend fun updateSiswa (siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
@@ -14,4 +17,5 @@ class OfflineRepositoriSiswa(
 ): RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+
 }
